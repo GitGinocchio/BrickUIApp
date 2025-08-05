a1dbfc46a266dd8b7e66489ac363ff4b39b83667
GitGinocchio <giuliotognetto2@gmail.com>
2025-08-05 21:00:04 +0200

    Initial commit

---
+++ src-tauri/gen/android/app/src/main/java/com/giulio_tognetto/brickui/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.giulio_tognetto.brickui
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+
+class MainActivity : TauriActivity() {
+  override fun onCreate(savedInstanceState: Bundle?) {
+    enableEdgeToEdge()
+    super.onCreate(savedInstanceState)
+  }
+}
